a0c20d80be7f048d0afcd1c88b0a7b3426bfb414
Saad Nadeem <saadnadeem5509@gmail.com>
2026-06-14 15:29:50 +0500

    connected main code with infra

--- main-code/terraform.tf
+++ main-code/terraform.tf
@@ -5,4 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tf-backup-bucket-sn-7223"
+    region = "eu-central-1"
+    key    = "terraform.tfstate"
+    dynamodb_table = "tf_backup_db"
+    
+  }
 }
