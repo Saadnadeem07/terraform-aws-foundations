66f77efc1f2b43443e4c4acc94b9225204687033
Saad Nadeem <saadnadeem5509@gmail.com>
2026-06-16 01:52:27 +0500

    fixed strings

--- multi-env/main.tf
+++ multi-env/main.tf
@@ -1,32 +1,32 @@
 module "dev-infra" {
-    env = dev
+    env = "dev"
     source = "./infra"
     region = "eu-central-1"
     instance_count = 2
     instance_type = "t2.micro"
-    ami_id = ami-0303e2e4a29f041a3
+    ami_id = "ami-0303e2e4a29f041a3"
     volume_size = 8
     volume_type = "gp3"
 }
 
 module "prod-infra" {
-    env = prod
+    env = "prod"
     source = "./infra"
     region = "eu-central-1"
     instance_count = 2
     instance_type = "t2.micro"
-    ami_id = ami-0303e2e4a29f041a3
+    ami_id = "ami-0303e2e4a29f041a3"
     volume_size = 8
     volume_type = "gp3"
 }
 
 module "staging-infra" {
-    env = staging
+    env = "staging"
     source = "./infra"
     region = "eu-central-1"
     instance_count = 2
     instance_type = "t2.micro"
-    ami_id = ami-0303e2e4a29f041a3
+    ami_id = "ami-0303e2e4a29f041a3"
     volume_size = 8
     volume_type = "gp3"
 }
